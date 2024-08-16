c587e5353fad2f63f8226d855a9313ae029abd37
axcelpinzon <157549403+AxcelPinzon@users.noreply.github.com>
2024-08-16 17:33:19 +0000

    tapos na

---
+++ CardLayuout.jsx
@@ -0,0 +1,9 @@
+// export default function CardLayout{ ()
+//     return (
+//         <div className="hover:bg-black p-5 bg-neutral-700 text-white rounded-lg">
+//           <img src="./vite.svg" className="w-20 h-20" />
+//           <h1 className="font-bold">Title</h1>
+//           <p className="tet-sm">Details</p>
+//         </div>
+//     );
+// }
