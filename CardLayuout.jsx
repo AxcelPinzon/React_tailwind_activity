a9a1640138d9e4d336dd0110757920874cbbbe76
axcelpinzon <157549403+AxcelPinzon@users.noreply.github.com>
2024-08-17 07:09:07 +0000

    sana

--- CardLayuout.jsx
+++
@@ -1,9 +0,0 @@
-// export default function CardLayout{ ()
-//     return (
-//         <div className="hover:bg-black p-5 bg-neutral-700 text-white rounded-lg">
-//           <img src="./vite.svg" className="w-20 h-20" />
-//           <h1 className="font-bold">Title</h1>
-//           <p className="tet-sm">Details</p>
-//         </div>
-//     );
-// }
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Card from "./components/CardLayout";
 function App() {
   return (
     <>
-      <main className="w-screen h-screen overflow-y-scroll overflow-hidden bg-[url('./Nike-logo.webp')] ">
-        <section className="flex flex-col justify-center items-center w-screen h-screen "></section>
+      <main className="w-screen h-screen overflow-y-scroll overflow-hidden flex justify-center items-center bg-[url('./Nike-logo.webp')] ">
+        <section className="flex flex-col justify-center items-center w-full h-screen "></section>
         <Hero />
         <section className="flex space-x-4 justify-center items-center w-screen h-screen bg-[url('./Nike-logo.webp')]"></section>
         <Card />
