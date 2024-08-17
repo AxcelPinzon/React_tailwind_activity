d8f4635e5345a0bfc143ab6d0f236d858224e799
axcelpinzon <157549403+AxcelPinzon@users.noreply.github.com>
2024-08-17 07:29:36 +0000

    djuasfhas

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,19 +2,16 @@ export default function HeroSection() {
   return (
     <div className="max-w-screen-lg mx-auto text-center">
       <header className="my-32">
-        <h1 className="my-8 text-6xl font-bold uppercase">Just Do it</h1>
-        <p className="text-xl leading-loose">
+        <h1 className="my-8 text-6xl font-bold uppercase text-white">
+          Just Do it
+        </h1>
+        <p className="text-xl leading-loose text-white">
           Any Place. Any Time. Any Condition. Be Your Best Every Time With Nike
           Shoes And Clothing. The Official Website. Home Of Everything Nike.
           Shop The Latest Releases Today. Styles: Mens, Womens, Kids, Boys,
           Girls.
         </p>
-        <a
-          href="#projects"
-          className="inline-block mt-8 bg-indigo-500 text-white px-16 py-3 shadow rounded font-bold uppercase transition duration-300 ease-in hover:-translate-y-1"
-        >
-          Projects
-        </a>
+        <a className="inline-block mt-8 bg-indigo-500 text-white px-16 py-3 shadow rounded font-bold uppercase transition duration-300 ease-in hover:-translate-y-1"></a>
       </header>
     </div>
   );
